965952bc9111d8449cc139e1c59aa8143b629ace
charley-xiao <charleyxiao057@gmail.com>
2023-05-30 19:39:43 +0800

    Site updated: 2023-05-30 19:39:43

---
+++ showgrades.js
@@ -0,0 +1,25 @@
+var failCnt=0;
+var f22validated=false;
+function validatef22(){
+	if(failCnt>3){
+		window.alert('Too many requests!');
+		return;
+	}
+	if(f22validated){
+		document.getElementById('f22').innerHTML='GPA: 2.96/4.00 Rank: 1087/1371. Click <a style="color: blue;" onclick="cancelf22()">here</a> to hide the content.';
+		return;
+	}
+	var pswd=window.prompt('Enter password to check grades for 2022 Fall');
+	if(pswd=='adminf22validated'){
+		f22validated=true;
+		document.getElementById('f22').innerHTML='GPA: 2.96/4.00 Rank: 1087/1371. Click <a style="color: blue;" onclick="cancelf22()">here</a> to hide the content.';
+	}
+	else{
+		window.alert('Wrong password!');
+		failCnt+=1;
+	}
+	return;
+}
+function cancelf22(){
+	document.getElementById('f22').innerHTML=' Privileged information, please click <a style="color: blue;" onclick="validatef22()">here</a> and input password to gain access.';
+}
